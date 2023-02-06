170f88474b161c7d62bc473e9ce00343bc1e5142
Luis Plaza <plaza3573@gmail.com>
2023-02-05 20:59:03 -0500

    Initiate the home screen

--- app/src/main/java/com/example/giftly/HomeScreen.java
+++ app/src/main/java/com/example/giftly/HomeScreen.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class HomeScreen extends AppCompatActivity {
 
+    //Variables for profile icon
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
