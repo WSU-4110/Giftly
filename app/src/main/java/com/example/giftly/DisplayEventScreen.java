8dfaac50ad1778f0db6b1bcff20afea8517fd6a4
hannab8 <hg3805@wayne.edu>
2023-02-09 23:02:30 -0500

    Initial Display Events Screen Commit

---
+++ app/src/main/java/com/example/giftly/DisplayEventScreen.java
@@ -0,0 +1,20 @@
+package com.example.giftly;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class DisplayEventScreen extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_event);
+
+
+        //TextView event_title=(TextView) findViewById(R.id.event_name);
+        //event_title.setText();
+
+    }
+}
+
