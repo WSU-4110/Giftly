447e5b58a7dbbd8e36fcba9c5eb0bbb597c20872
Luis Plaza <plaza3573@gmail.com>
2023-02-12 13:08:26 -0500

    Home and Profile setting screens

--- app/src/main/java/com/example/giftly/HomeScreen.java
+++ app/src/main/java/com/example/giftly/HomeScreen.java
@@ -1,8 +1,12 @@
 package com.example.giftly;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menu_item_profile){
+            startActivity(new Intent(HomeScreen.this, ProfileScreen.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/giftly/SignUpScreen.java
+++ app/src/main/java/com/example/giftly/SignUpScreen.java
@@ -31,7 +31,6 @@ public class SignUpScreen extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
     private TextView txtLoginInfo;
-    DB_Helper DB;
     private boolean isSigningUp = true;
 
     @Override
@@ -39,6 +38,8 @@ public class SignUpScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_screen);
 
+        //Real-time database management
+        /*
         mAuth = FirebaseAuth.getInstance();
         firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -48,7 +49,7 @@ public class SignUpScreen extends AppCompatActivity {
                 finish();
                 return;
             }
-        };
+        };*/
         
         editName = (EditText) findViewById(R.id.editUsername);
         editPassword = (EditText) findViewById(R.id.editPassword);
@@ -56,6 +57,13 @@ public class SignUpScreen extends AppCompatActivity {
         btnSubmit = findViewById(R.id.btnSubmit);
         txtLoginInfo = findViewById(R.id.txtLoginInfo);
         editInterests = (EditText) findViewById(R.id.userInterests);
+
+        //Auto sign in if the user hasn't logged out
+        /*
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null){
+            startActivity(new Intent(SignUpScreen.this, HomeScreen.class));
+            finish();
+        }*/
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -67,11 +75,13 @@ public class SignUpScreen extends AppCompatActivity {
                     isSigningUp = false;
                     //Hide the visibility of the text
                     editName.setVisibility(View.GONE);
+                    editInterests.setVisibility(View.GONE);
                     btnSubmit.setText("Log in");
                     txtLoginInfo.setText("Dont have an account? Sign up");
                 }else{
                     isSigningUp = true;
                     editName.setVisibility(View.VISIBLE);
+                    editInterests.setVisibility(View.VISIBLE);
                     btnSubmit.setText("Sign up");
                     txtLoginInfo.setText("Already have an account? Log in");
                 }
@@ -97,35 +107,41 @@ public class SignUpScreen extends AppCompatActivity {
 
     }
 
+    //Signing in exceptions and configurations
     private void handleSignUp(){
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(editEmail.getText().toString(),editPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
+                    //After signing up, transition to the home screen
+                    startActivity(new Intent(SignUpScreen.this, HomeScreen.class));
                     Toast.makeText(SignUpScreen.this, "Signed in successfully", Toast.LENGTH_SHORT).show();
+
+                    //Real-time database management
+                    /*
                     String user_id = mAuth.getCurrentUser().getUid();
                     DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
-
                     String name = editName.getText().toString();
                     String interests = editInterests.getText().toString();
-
                     Map newPost = new HashMap();
                     newPost.put("name", name);
                     newPost.put("interests",interests);
-
-                    current_user_db.setValue(newPost);
+                    current_user_db.setValue(newPost);*/
                 }else{
                     Toast.makeText(SignUpScreen.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-
                 }
             }
         });
     }
+
+    //Logging in exception and configurations
     private void handleLogin(){
         FirebaseAuth.getInstance().signInWithEmailAndPassword(editEmail.getText().toString(),editPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
+                    //After logging in, transition to the home screen
+                    startActivity(new Intent(SignUpScreen.this, HomeScreen.class));
                     Toast.makeText(SignUpScreen.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
                 }else{
                     Toast.makeText(SignUpScreen.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
@@ -134,6 +150,7 @@ public class SignUpScreen extends AppCompatActivity {
         });
     }
 
+    //Back button configuration
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
