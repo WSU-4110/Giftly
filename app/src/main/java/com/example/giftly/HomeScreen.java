abd059b7df9de7996b7c9abcca0da2574324ae6c
Cadek2002 <77899221+Cadek2002@users.noreply.github.com>
2023-02-12 14:16:25 -0500

    Added functionality for requesting and receiving lists of user objects from db

--- app/src/main/java/com/example/giftly/HomeScreen.java
+++ app/src/main/java/com/example/giftly/HomeScreen.java
@@ -1,6 +1,8 @@
 package com.example.giftly;
 import static android.content.ContentValues.TAG;
 
+import static com.example.giftly.Giftly.client;
+
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +19,8 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.firebase.firestore.DocumentSnapshot;
 
+import java.util.ArrayList;
+
 public class HomeScreen extends AppCompatActivity {
 
     @Override
@@ -26,16 +30,35 @@ public class HomeScreen extends AppCompatActivity {
 
         //find Users name and display it to test readUser data
         TextView display = findViewById(R.id.TestDisplay);
-        ListenableFuture<User> getUser =  Giftly.client.readUser(FireBaseClient.getAuth().getUid());
 
         Futures.addCallback(
-                getUser,
+                client.readUser(client.getAuth().getUid()),
                 new FutureCallback<User>() {
                     public void onSuccess(User result) {
                         // handle success
                         display.setText(result.getFullName());
                     }
 
+                    public void onFailure(@NonNull Throwable thrown) {
+                        // handle failure
+                        //kill the app idk
+                    }
+                },
+                // causes the callbacks to be executed on the main (UI) thread
+                this.getMainExecutor()
+        );
+
+        ArrayList<String> users = new ArrayList<>(2);
+        users.add("WmappNZE1YYqCQzguJ26leS3vuz1");
+        users.add("iCywYGeu0ycde16LTr4zvR6Fivx1");
+
+        Futures.addCallback(
+                client.readUser(users),
+                new FutureCallback<ArrayList<User>>() {
+                    public void onSuccess(ArrayList<User> result) {
+                        // handle success
+                        display.setText(result.get(1).getFullName());
+                    }
                     public void onFailure(@NonNull Throwable thrown) {
                         // handle failure
                         //kill the app idk
