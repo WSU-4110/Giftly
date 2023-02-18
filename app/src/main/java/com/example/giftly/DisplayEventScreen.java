1b28f0ebb9c5516ac54dc1cf889535478b769547
hannab8 <hg3805@wayne.edu>
2023-02-17 23:37:27 -0500

    Updated Display Event Page.

--- app/src/main/java/com/example/giftly/DisplayEventScreen.java
+++ app/src/main/java/com/example/giftly/DisplayEventScreen.java
@@ -1,20 +1,32 @@
 package com.example.giftly;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class DisplayEventScreen extends AppCompatActivity {
 
+    // Temporary List of Participants; array to be filled in by database info
+    ListView list_1;
+    String participants[]
+            ={"Cayden", "Luis", "Matt", "Noura", "Hanna"};
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_event);
 
-
-        //TextView event_title=(TextView) findViewById(R.id.event_name);
-        //event_title.setText();
-
+        // The list of participants is displayed on the screen
+        list_1 = findViewById(R.id.participant_list);
+        ArrayAdapter<String> arr;
+        arr = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,participants);
+        list_1.setAdapter(arr);
     }
 }
 
