5fae3a5e74ecaa6ebf59afc075519e039fc79909
Noura <aoura.nlaboudi@gmail.com>
2023-04-05 13:38:33 -0400

    Back button integration on layout screen & minor UI changes

--- app/src/main/java/com/example/giftly/LayoutScreen.java
+++ app/src/main/java/com/example/giftly/LayoutScreen.java
@@ -1,5 +1,7 @@
 package com.example.giftly;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -7,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -49,7 +52,19 @@ public class LayoutScreen extends AppCompatActivity {
             }
         });
 
+        //The action bar will display the back button in the header
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
-
+    }
+    //Back button configuration
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
