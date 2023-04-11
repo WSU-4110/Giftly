a9cf751640448b540fa8a2f3d20efe2046d2e18a
Noura <aoura.nlaboudi@gmail.com>
2023-04-11 01:50:16 -0400

    Naming conventions for file names

--- app/src/main/java/com/example/giftly/ThemeScreen.java
+++ app/src/main/java/com/example/giftly/ThemeScreen.java
@@ -1,22 +1,14 @@
 package com.example.giftly;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 //private SharedPreferences sharedPreferences;
@@ -28,7 +20,7 @@ public class CThemeScreen extends AppCompatActivity implements ThemeObserver {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_ctheme_screen);
+            setContentView(R.layout.theme_screen);
 
             themeSubject = new ThemeSubject();
             themeSubject.registerObserver(this);
--- app/src/main/java/com/example/giftly/eventViewer.java
+++ app/src/main/java/com/example/giftly/eventViewer.java
@@ -10,14 +10,14 @@ import com.example.giftly.handler.IEvent;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 
-public class crappyEventViewer extends AppCompatActivity {
+public class eventViewer extends AppCompatActivity {
     TextView displayTest = findViewById(R.id.testDisplay);
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crappy_event_viewer);
+        setContentView(R.layout.event_viewer);
 
         Intent intent = getIntent();
         String eventID = intent.getStringExtra("eventID");
